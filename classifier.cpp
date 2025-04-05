6be9010a368ffc99bab66a0698f170d54f342ceb
Shumroz Usmani <shrezorhere2002@gmail.com>
2025-04-04 23:36:05 -0400

    Add train function to classifier.cpp

--- classifier.cpp
+++ classifier.cpp
@@ -4,13 +4,3 @@ void train() {
     printf("train\n");
 }
 
-void predict() {
-    printf("predict\n");
-}
-
-int main() {
-    // Call the functions to test
-    train();
-    predict();
-    return 0;
-}
