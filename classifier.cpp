5e906dc24879eea0c52fdb64ab6b07d74d603173
Shumroz Usmani <shrezorhere2002@gmail.com>
2025-04-04 20:56:19 -0400

    Add classifier.cpp with train and predict functions

---
+++ classifier.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void train() {
+    printf("train\n");
+}
+
+void predict() {
+    printf("predict\n");
+}
+
+int main() {
+    // Call the functions to test
+    train();
+    predict();
+    return 0;
+}
